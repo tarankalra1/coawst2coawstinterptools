f96368af6c27660564801df97385cd32d33645c2
Tarandeep Singh Kalra <tarankalra@gmail.com>
2020-04-27 18:37:30 -0400

    folderized

--- rho2u_2d_mai.m
+++
@@ -1,7 +0,0 @@
-function var_u=rho2u_2d_mai(var_rho);
-
-[Lp,Mp]=size(var_rho);
-M=Mp-1;
-var_u=0.5*(var_rho(:,1:M)+var_rho(:,2:Mp));
-
-return
--- rho2v_2d_mai.m
+++
@@ -1,7 +0,0 @@
-function var_v=rho2v_2d_mai(var_rho);
-
-[Lp,Mp]=size(var_rho);
-L=Lp-1;
-var_v=0.5*(var_rho(1:L,:)+var_rho(2:Lp,:));
-
-return
